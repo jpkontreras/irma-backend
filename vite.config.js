5fd26e963273d8109191f7a211075ffeb9c59ed4
Julio <julio.contreras.insuco@gmail.com>
2024-10-16 23:43:21 -0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    server: {
+        host: '127.0.0.1', // or 'localhost'
+        port: 5174
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.tsx',
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+});
