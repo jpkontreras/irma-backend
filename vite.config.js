e0633ceaa964fe8964f8e6cc989928780029c3ef
Julio <julio.contreras.insuco@gmail.com>
2024-10-19 00:08:27 +0000

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,19 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    server: {
-        host: '127.0.0.1', // or 'localhost'
-        port: 5174
-    },
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
             ssr: 'resources/js/ssr.tsx',
-            refresh: true,
+            refresh: true
         }),
         react(),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            clientPort: 5173,
+            host: 'localhost',
+        },
+    }
 });
