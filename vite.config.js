e37c67375cca5b1706b5f33e73bc74d52ed20cd8
Julio <julio.contreras.insuco@gmail.com>
2024-10-24 20:19:47 +0000

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
-import laravelTranslator from 'laravel-translator/vite'
+import laravelTranslator from 'laravel-translator/vite';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.tsx',
-            ssr: 'resources/js/ssr.tsx',
-            refresh: true
-        }),
-        react(),
-        laravelTranslator()
-    ],
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            clientPort: 5173,
-            host: 'localhost',
-        },
-    }
+  plugins: [
+    laravel({
+      input: 'resources/js/app.tsx',
+      ssr: 'resources/js/ssr.tsx',
+      refresh: true,
+    }),
+    react(),
+    laravelTranslator(),
+  ],
+  server: {
+    host: '0.0.0.0',
+    hmr: {
+      clientPort: 5173,
+      host: 'localhost',
+    },
+  },
 });
